3289fe07abe9662203d69ef5de0a7aa3c954b6f7
Muflih-uk <muhammedmuflih875@gmail.com>
2026-01-02 22:29:57 +0530

    feat: Nav Bar Created

---
+++ src/components/ui/NavBar.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { ArrowLeft, Heart } from "lucide-react";
+import { useState } from "react";
+
+const NavBar : React.FC = () => {
+
+    const [isLiked, setLiked] = useState(false);
+
+    const onPressed = () => {
+        setLiked(!isLiked);
+    };
+
+    return(
+        <div className="w-full">
+            <div className="flex justify-between w-full p-5">
+                <div className="bg-white p-2 rounded-full">
+                    <ArrowLeft/>
+                </div>
+                <button onClick={onPressed}>
+                    <Heart fill={isLiked ? "red" : "#FFFFFF"} className={`text-red-700`}/>
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
