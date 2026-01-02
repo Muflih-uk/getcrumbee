664afa0e33d4c61f6c20a89c52ca95539220d4d8
Muflih-uk <muhammedmuflih875@gmail.com>
2026-01-02 22:29:24 +0530

    feat: Counter Component Created

---
+++ src/components/ui/Counter.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { useState } from "react";
+import { Minus, Plus } from "lucide-react";
+
+const Counter : React.FC = () => {
+    
+    const [count, setCount] = useState(0);
+    
+    const increment = () => {
+        if (count < 20) {
+        setCount(count + 1);
+        }
+    };
+
+    const decrement = () => {
+        if (count > 0) {
+        setCount(count - 1);
+        }
+    };
+    return(
+        <div className="rounded-full p-2 shadow-xl gap-3 flex items-center">
+            <button
+                onClick={decrement}
+                disabled={count==0} 
+                className="cursor-pointer bg-(--secondary-color) rounded-full p-2"
+            >
+                <Minus/>
+            </button>
+            <div className="font-bold text-xl">{count}</div>
+            <button
+                onClick={increment} 
+                className="cursor-pointer bg-(--primary-color) rounded-full p-2"
+            >
+                <Plus className="text-white"/>
+            </button>
+        </div>
+    );
+}
+
+export default Counter;
